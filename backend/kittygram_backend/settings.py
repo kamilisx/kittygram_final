74e55c69f8b35a79aac9f8422be5d1b30f6daacf
Камиль Исхаков <kamiliskhakov@MacBook-Air-Kamil.local>
2023-11-10 21:14:29 +0400

    Add Actions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET!')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['158.160.28.92', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
